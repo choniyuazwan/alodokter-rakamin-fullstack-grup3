f8691185960d08bb44493b174c60d8d5859ca189
choniyuazwan <choniyuazwan@gmail.com>
2021-12-09 15:31:43 +0700

    feat: add register login with jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/Authentication'
+  namespace :api do
+    namespace :v1 do
+      # resources :categories, only: %i[index create destroy]
+      # resources :books, only: %i[index create show update destroy]
+
+      post 'login', to: 'authentication#create'
+      post 'register', to: 'users#create'
+    end
+  end
 end
---
+++ db/migrate/20211209064250_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :firstname
+      t.string :lastname
+      t.string :birthdate
+      t.string :gender
+      t.string :phone
+      t.string :idendity
+      t.string :address
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
