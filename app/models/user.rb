172fe6c1bc6ce12f5f996f547b54dd8700e32579
choniyuazwan <choniyuazwan@gmail.com>
2021-12-10 18:45:14 +0700

    fix: password cant blank when edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   # has_many :books
   validates :email, presence: true, uniqueness: true, length: { minimum: 3 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
